4310abcdaf5fa8ca5232ca1451719a662d186173
vektor8891 <viktor.szabo123@gmail.com>
2020-12-16 00:41:33 -0500

    feature: modulo operator

--- src/data/number/numberFn.ts
+++ src/data/number/numberFn.ts
@@ -4,3 +4,12 @@ import { PLNumber } from './PLNumber'
 export const plNumber = (value: number): PLNumber => new PLNumber(value)
 
 export const parseNumber = (value: string): PLNumber => PLNumber.fromStr(plString(value))
+
+export function modulo(dividend: PLNumber, divisor: PLNumber): PLNumber {
+  const remainder = dividend.value % divisor.value
+  return plNumber(remainder)
+}
+
+export default {
+  modulo,
+}
