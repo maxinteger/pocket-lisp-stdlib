6075159c79d20f9f0de75fbe669a50f65fbbb9d5
vektor8891 <38993315+vektor8891@users.noreply.github.com>
2021-01-03 18:16:09 -0500

    Bugfix/gcd (#16)
    fix handling 0s in gcd

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -2,7 +2,7 @@
  * Greatest common divisor - Euclid's algorithm
  */
 export const gcd = (a: number, b: number): number => {
-  return a === b || !a || !b ? a : gcd(b, a % b)
+  return a === b || !a ? b : !b ? a : gcd(b, a % b)
 }
 
 export const isBelowEpsilon = (x: number): boolean => {
