fa9d70be27e3d84fdfc454ae8bc3207704ce1a7b
vektor8891 <38993315+vektor8891@users.noreply.github.com>
2021-09-30 08:24:15 -0400

    Update order of regexp flag

--- src/data/string/stringFn.spec.ts
+++ src/data/string/stringFn.spec.ts
@@ -17,9 +17,9 @@ describe('stringFn', () => {
 
   describe('replace-regexp', () => {
     it('should replace regexp string', () => {
-      expect(replaceRegexp(plString('Dog'), plString('i'), plString('monkey'), plString('dog'))).toEqual(plString('monkey'))
-      expect(replaceRegexp(plString('apples'), plString('gi'), plString('oranges'), plString('Apples are round, and apples are juicy.'))).toEqual(plString('oranges are round, and oranges are juicy.'))
-      expect(replaceRegexp(plString('(\\w+)\\s(\\w+)'), plString(''), plString('$2, $1'), plString('John Smith'))).toEqual(plString('Smith, John'))
+      expect(replaceRegexp(plString('i'), plString('Dog'), plString('monkey'), plString('dog'))).toEqual(plString('monkey'))
+      expect(replaceRegexp(plString('gi'), plString('apples'), plString('oranges'), plString('Apples are round, and apples are juicy.'))).toEqual(plString('oranges are round, and oranges are juicy.'))
+      expect(replaceRegexp(plString(''), plString('(\\w+)\\s(\\w+)'), plString('$2, $1'), plString('John Smith'))).toEqual(plString('Smith, John'))
     })
   })
 })
