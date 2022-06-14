6a0720fe5046624d65aa4b9f37e9b44d0fb19719
vektor8891 <vszabo@protonmail.com>
2022-06-14 02:59:22 -0400

    sort vectors

--- src/data/vector/vectorFn.ts
+++ src/data/vector/vectorFn.ts
@@ -58,6 +58,11 @@ export const reverse: <T extends PLBase>(list: PLVector<T>) => PLVector<T> = (li
   return plVector(...(list.value.reverse() as StrictArray<any>))
 }
 
+export const sort: <T extends PLBase>(list: PLVector<T>) => PLVector<T> = (list) => {
+  typeCheck(PLVector, list)
+  return plVector(...(list.value.sort() as StrictArray<any>))
+}
+
 export const slice: <T extends PLBase>(list: PLVector<T>, start: PLNumber, end: PLNumber) => PLVector<T> = (
   list,
   start,
@@ -80,5 +85,6 @@ export default {
   head,
   reverse,
   slice,
+  sort,
   tail,
 }
